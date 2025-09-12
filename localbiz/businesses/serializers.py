c49e3363a5ba99877feb38c222a81e9f807c110a
AmoKiswaya <amosonganga254@gmail.com>
2025-09-12 12:42:25 +0300

    Add url and update_url fields in BusinessSerializer and get_update_url method

--- localbiz/businesses/serializers.py
+++ localbiz/businesses/serializers.py
@@ -1,11 +1,38 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from .models import Business, Category
 
 class BusinessSerializer(serializers.ModelSerializer):
+    update_url = serializers.SerializerMethodField(read_only=True) 
+    url = serializers.HyperlinkedIdentityField(
+        view_name='business-detail',
+        lookup_field='pk'
+    )
+    
     class Meta:
         model = Business
-        fields = "__all__" 
-        read_only_fields = ["id", "owner", "created_at"]
+        fields = [
+            "url",
+            "update_url", 
+            "pk", 
+            "owner", 
+            "category", 
+            "name", 
+            "description", 
+            "location", 
+            "address", 
+            "website", 
+            "created_at",
+        ] 
+        
+        read_only_fields = ["pk", "owner", "created_at"]
+
+    def get_update_url(self, obj):
+        request = self.context.get('request') 
+        if request is None:
+            return None
+        return reverse("business-update", kwargs={"pk": obj.pk}, request=request) 
+
 
     def validate(self, data):
           if Business.objects.filter(
