5f9f106ce1653775bc0638e93918dcc999fe1b3d
AmoKiswaya <amosonganga254@gmail.com>
2025-09-18 17:54:57 +0300

    Rename url path for token verify in project urls file.

--- localbiz/localbiz/urls.py
+++ localbiz/localbiz/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path("api/", include("reviews.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
